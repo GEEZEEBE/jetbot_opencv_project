783ade0eea4f980915035b0424186df616c575d0
jakey22 <sunjeon7@gmail.com>
2020-12-17 17:45:26 +0900

    moment

--- Detecting line.py.py
+++ Detecting line.py.py
@@ -2,10 +2,11 @@
 import cv2 as cv 
 import numpy as np
 import os
+
 def make_canny(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray,(3,3),0)
-    canny = cv.Canny(blur,50,150) ## 
+    canny = cv.Canny(blur,55,90) ## 
     return canny 
 
 dir = "./snapshots_road"
@@ -14,18 +15,25 @@ for road in roads:
 
     image = cv.imread(dir + '/' + road)
     canny = make_canny(image)
-    # cv.imshow("canny", canny)
-    # cv.waitKey(0)
-  
-    contours, hierarchy = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE) 
+    cv.imshow("canny", canny)
+    contours, hierarchy = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
     for contour in contours:
-        
+
         area = cv.contourArea(contour)
         epsilon = 0.025*cv.arcLength(contour, True)
         approx = cv.approxPolyDP(contour, epsilon, True)
         if len(approx) == 4 and area > 50:
-            cv.drawContours(image, [approx], 0, (0,0,255), -1)
-            cv.imshow("image", image)
+
+            mmt = cv.moments(contour)
+            cx = int(mmt['m10']/mmt['m00'])
+            cy = int(mmt['m01']/mmt['m00'])
+
+            cv.circle(image, (cx,cy), 5, (255,0,0), -1)
+            cv.drawContours(image, [approx], 0, (0,0,255), 1)
+            
+            cv.imshow("canny", canny)
+            cv.imshow(f"{road}image", image)
             cv.waitKey(0)
 
             #https://github.com/NVIDIA-AI-IOT/jetbot/tree/master/notebooks
+            #
